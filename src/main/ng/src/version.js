e0d4ea856c796546e4536463efe69426a2e2c0ea
Anand Rockzz <anandchakru.forum@gmail.com>
2018-12-07 14:22:35 -0800

    fired package.json's version, hired version.js child_process & spawnSync

--- src/main/ng/src/version.js
+++ src/main/ng/src/version.js
@@ -1,22 +1,28 @@
-//npm --save-dev git-describe fs-extra
-const { gitDescribeSync } = require('git-describe');
-const { version } = require('../package.json');
+//git rev-list --tags --date-order --max-count=1
+//git describe --exact-match $(git rev-list --tags --date-order --max-count=1) --tags
+'use strict';
+const { spawnSync } = require('child_process');
 const { resolve, relative } = require('path');
 const { writeFileSync } = require('fs-extra');
 
-const gitInfo = gitDescribeSync({
-    dirtyMark: false,
-    dirtySemver: false
-});
-
-gitInfo.version = version;
+const hashCmd = spawnSync( 'git', [ 'rev-list','--tags','--date-order','--max-count=1' ] );
+const hash = hashCmd.stdout.toString().trim();
+const tagCmd = spawnSync( 'git', [ 'describe','--exact-match',hash,'--tags' ] );
+const op = {'hash':hash,'tag':tagCmd.stdout.toString().trim()};
 
 const file = resolve(__dirname, '..', 'src', 'environments', 'version.ts');
+
 writeFileSync(file,
 `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
+export const VERSION = ${JSON.stringify(op, null, 2)};
 /* tslint:enable */
 `, { encoding: 'utf-8' });
 
-console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
+console.log(`Wrote ${JSON.stringify(op)} to ${relative(resolve(__dirname, '..'), file)}`);
+if(hashCmd.stderr.toString()){
+    console.log(`hashError: ${hashCmd.stderr.toString()}`);
+}
+if(tagCmd.stderr.toString()){
+    console.log(`tagError: ${tagCmd.stderr.toString()}`);
+}
